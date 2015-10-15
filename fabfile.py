5149fe661dac229e249aee1344657169a9ed6c22
diegodefuentes <diego.fuentes@softtek.com>
2015-10-15 11:54:46 -0500

    Adding fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,9 @@
+from fabric.api import put
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import task
+
+@task
+def provisionServer():
+    sudo('apt-get install apache2')
+	sudo('service apache2 restart')
