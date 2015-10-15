058a78df4faba26814c5b0610a9437895e6846df
diegodefuentes <diego.fuentes@softtek.com>
2015-10-15 12:50:33 -0500

    Add cookbook

--- fabfile.py
+++ fabfile.py
@@ -7,18 +7,18 @@ from fabric.api import task
 def provisionServer():
     sudo('curl -L https://www.chef.io/chef/install.sh | sudo bash')
     
-	run('rm -rf cookbooks/')
-	run('mkdir -p cookbooks')
+    run('rm -rf cookbooks/')
+    run('mkdir -p cookbooks')
     
-	put('trapache', 'cookbooks')
+    put('trapache', 'cookbooks')
     run('ls -lah cookbooks')
 	
     run('rm -rf cookbooks.tar.gz')
     run('tar cvzpf cookbooks.tar.gz cookbooks/')
-	#put es como scp
-	put('runlist.json', 'runlist.json')
+    #put es como scp
+    put('runlist.json', 'runlist.json')
 	
-	sudo('chef-solo -j runlist.json -r cookbooks.tar.gz')
+    sudo('chef-solo -j runlist.json -r cookbooks.tar.gz')
 	
 @task
 def deployIndex():
